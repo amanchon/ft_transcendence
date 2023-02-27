0aca778f0a007db7fc35204aaa8d08815cbedec1
hugo <h.eynard@orange.fr>
2023-02-27 14:23:06 +0100

    remove useless repo

--- services/PostgreSQL/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-systemctl restart postgresql
-#####################
-
-echo "CREATE USER pong WITH ENCRYPTED PASSWORD 'pong' NOCREATEDB;" | sudo -u postgres psql
-echo "CREATE DATABASE "DB_PONG" WITH OWNER='pong';" | sudo -u postgres psql
-cat /database.sql | sudo -u postgres psql
